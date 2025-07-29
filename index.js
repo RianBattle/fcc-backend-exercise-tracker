f1a0535f287ab26521a2576bd4057cc839b41f3d
Rian Battle <89739973+RianBattle@users.noreply.github.com>
2025-07-29 15:17:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,13 +5,48 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+class User {
+  static nextId = 1;
+  constructor(username) {
+    this._id = User.nextId;
+    User.nextId += 1;
 
+    this.username = username;
+    console.log("username:", username);
+    this.log = [];
+  }
+}
 
+class LogEntry {
+  constructor(description, duration, date) {
+    this.description = description;
+    this.duration = duration;
+    this.date = date;
+  }
+}
 
+const users = [];
+
+app.route("/api/users")
+  .get((req, res) => res.json(users.map(u => ({
+    _id: u._id,
+    username: u.username
+  }))))
+  .post((req, res) => {
+    const newUser = new User(req.body.username);
+    users.push(newUser);
+    res.json({
+      _id: newUser._id,
+      username: newUser.username
+    });
+  });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
